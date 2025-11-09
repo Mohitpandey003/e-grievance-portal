65278439c30b17d89c874741ee867baf8d07cdbf
Mohitpandey003 <mohitraj2022003@gmail.com>
2025-11-09 13:02:55 +0530

    updated backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL of your backend
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://e-grievance-portal.vercel.app/api';
 
 // Create axios instance with default config
 const api = axios.create({
