247d5e37f13b134b7143d01f6351d48f84f7bf67
Mohitpandey003 <mohitraj2022003@gmail.com>
2026-05-21 13:01:53 +0530

    Added Vite env variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL of your backend
-const API_URL = 'https://e-grievance-portal-1.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance with default config
 const api = axios.create({
